75e6eeb568fe5456e2209332ef6f73779208e9fd
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-06 14:43:16 +0300

    Entity sınıflarımız oluşturuldu. Başlangıçta 10 entity sınıfı eklendi.

---
+++ EntityLayer/Concrete/About2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About2
+    {
+        public int About2ID { get; set; }
+        public string Title1 { get; set; }
+        public string Title2 { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
