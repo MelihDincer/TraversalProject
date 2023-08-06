aef56616772eec3b00b6163a899a09728539b5fc
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-06 16:34:06 +0300

    Sınıf ismi revize edildi. Ayrıca key attribute u tanımlandı.

--- EntityLayer/Concrete/Destination.cs
+++ EntityLayer/Concrete/Destination.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-    public class Destinations
+    public class Destination
     {
+        [Key]
         public int DestinationID { get; set; }
         public string City { get; set; }
         public string DayNight { get; set; }
