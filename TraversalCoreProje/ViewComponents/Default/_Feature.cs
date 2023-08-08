effba5df24f404512d23921bc76872032609bcdf
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-08 22:17:03 +0300

    Feature viewcomponent class oluşturuldu.

---
+++ TraversalCoreProje/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _Feature: ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = featureManager.TGetList().Take(4).ToList();
+            return View(values);
+        }
+    }
+}
