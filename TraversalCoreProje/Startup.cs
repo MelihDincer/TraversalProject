b858df5808067dee599e6af9ac049fc9682a5790
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-08 22:36:03 +0300

    Projenin default olan MapControllerRoute'u düzenlendi.

--- TraversalCoreProje/Startup.cs
+++ TraversalCoreProje/Startup.cs
@@ -50,7 +50,7 @@ namespace TraversalCoreProje
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Default}/{action=Index}/{id?}");
             });
         }
     }
