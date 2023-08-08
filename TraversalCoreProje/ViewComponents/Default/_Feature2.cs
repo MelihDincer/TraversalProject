4a80db0abe17aa2a9d0dc743bf65818388dea400
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-08 22:17:13 +0300

    Feature2 viewcomponent class oluşturuldu.

---
+++ TraversalCoreProje/ViewComponents/Default/_Feature2.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _Feature2: ViewComponent
+    {
+        Feature2Manager feature2Manager = new Feature2Manager(new EfFeature2Dal());
+        public IViewComponentResult Invoke()
+        {
+            var values = feature2Manager.TGetList().Take(1).ToList();
+            return View(values);
+        }
+    }
+}
