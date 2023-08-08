8fe634510f7a3f1a8d4b89e8169c75f8ba7b9ae2
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-08 22:34:15 +0300

    Feature2Manager oluşturuldu.

---
+++ BusinessLayer/Concrete/Feature2Manager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class Feature2Manager : IFeature2Service
+    {
+        IFeature2Dal _feature2Dal;
+
+        public Feature2Manager(IFeature2Dal feature2Dal)
+        {
+            _feature2Dal = feature2Dal;
+        }
+
+        public void TAdd(Feature2 t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Feature2 t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Feature2 TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Feature2> TGetList()
+        {
+            return _feature2Dal.GetList();
+        }
+
+        public void TUpdate(Feature2 t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
