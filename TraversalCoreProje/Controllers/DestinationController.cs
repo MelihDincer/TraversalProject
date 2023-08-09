1753e2a7a9b68a62e3e6c68c168612127cd76a1a
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-09 10:24:15 +0300

    DestinationController oluşturuldu.

---
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+            ViewBag.v = id;
+            var values = destinationManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination p)
+        {
+            //var values = destinationManager.TGetByID(p.DestinationID);
+            //destinationManager.TUpdate(values);
+            return View();
+        }
+    }
+}
