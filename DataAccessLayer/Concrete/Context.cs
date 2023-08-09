f668dc45377dda73b4c85de6a3a3ba78730bde03
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-09 21:16:31 +0300

    Context sınıfı identity kütüphanesiyle birlikte revize edildi.

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
 ﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete
 {
-    public class Context : DbContext
+    public class Context : IdentityDbContext<AppUser, AppRole, int> //Sonradan manuel olarak eklediğimiz AppUser ve AppRole class larını burada kullanmış olduk.
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
