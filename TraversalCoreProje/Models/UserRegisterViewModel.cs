67740154eb14e8e5eeb5735dceb33473569df790
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-10 23:53:57 +0300

    Kayıt olma işlemi için viewmodel oluşturuldu.

---
+++ TraversalCoreProje/Models/UserRegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProje.Models
+{
+	public class UserRegisterViewModel
+	{
+        [Required(ErrorMessage = "Lütfen adınızı giriniz")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Lütfen soyadınızı giriniz")]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "Lütfen kullanıcı adınızı giriniz")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Lütfen mail adresinizi giriniz")]
+        public string Mail { get; set; }
+        [Required(ErrorMessage = "Lütfen şifreyi giriniz")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Lütfen şifreyi tekrar giriniz")]
+        [Compare("Password",ErrorMessage = "Şifreler eşleşmemektedir!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
