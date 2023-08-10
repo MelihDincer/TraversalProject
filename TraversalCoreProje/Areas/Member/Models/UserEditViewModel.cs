38668ad83933bdb51837c37ce1766ee0093d939c
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-10 23:11:33 +0300

    Kullanıcılar için UserEditViewModel oluşturuldu.

---
+++ TraversalCoreProje/Areas/Member/Models/UserEditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TraversalCoreProje.Areas.Member.Models
+{
+    public class UserEditViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Mail { get; set; }
+        public string ImageUrl { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
