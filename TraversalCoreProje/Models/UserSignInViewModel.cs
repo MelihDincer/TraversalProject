086aceedb8d149d431c14ed8cf320744935ab274
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-10 23:53:40 +0300

    Login işlemi için viewmodel oluşturuldu.

---
+++ TraversalCoreProje/Models/UserSignInViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProje.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage ="Burayı boş geçemezsiniz...")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Burayı boş geçemezsiniz...")]
+        public string Password { get; set; }
+    }
+}
