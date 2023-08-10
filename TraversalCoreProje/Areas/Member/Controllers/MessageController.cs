b6eb815ae9f07d2d28bda42ef358646cdf0d07cd
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-10 23:10:13 +0300

    Area'da MessageController oluşturuldu.

---
+++ TraversalCoreProje/Areas/Member/Controllers/MessageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class MessageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
