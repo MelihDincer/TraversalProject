b2a158bcb26e929641f1d56a48423d10544d8275
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-11 23:48:05 +0300

    IReservationService oluşturuldu. Onay bekleyen-onaylanan ve geçmiş rezervasyonlar için metot imzaları eklendi.

---
+++ BusinessLayer/Abstract/IReservationService.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IReservationService : IGenericService<Reservation>
+    {
+        List<Reservation> GetListWithReservationByWaitApproval(int id); //Onay bekleyen rezervasyonlar listesi
+        List<Reservation> GetListWithReservationByAccepted(int id); //Onaylanan rezervasyonlar listesi
+        List<Reservation> GetListWithReservationByPrevious(int id); //Geçmiş rezervasyonlar listesi
+
+    }
+}
