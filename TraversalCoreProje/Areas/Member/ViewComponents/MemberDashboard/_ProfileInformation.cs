c62b633735f84c2ed9d3039cc7f91f617e8313f2
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-11 23:57:04 +0300

    _ProfileInformation  viewcomponent class oluşturuldu.

---
+++ TraversalCoreProje/Areas/Member/ViewComponents/MemberDashboard/_ProfileInformation.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Member.ViewComponents.MemberDashboard
+{
+    public class _ProfileInformation : ViewComponent
+    {
+        //private readonly UserManager<AppUser> _userManager;
+
+        //public _ProfileInformation(UserManager<AppUser> userManager)
+        //{
+        //    _userManager = userManager;
+        //}
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
