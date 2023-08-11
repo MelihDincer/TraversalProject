29144e8b588564fdfc6bbf3fccdc5cc2d69d6d8b
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-11 23:49:29 +0300

    IReservationDal oluşturuldu.

---
+++ DataAccessLayer/Abstract/IReservationDal.cs
@@ -0,0 +1,16 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IReservationDal : IGenericDal<Reservation>
+    {
+        List<Reservation> GetListWithReservationByWaitApproval(int id); //Onay bekleyen rezervasyonları getir.
+        List<Reservation> GetListWithReservationByAccepted(int id); //Kabul edilen rezervasyonları getir.
+        List<Reservation> GetListWithReservationByPrevious(int id); //Geçmiş rezervasyonları getir.
+    }
+}
