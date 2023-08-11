975c5c6d3c9ea79fe07c2fa7c61bfe193ea5fa25
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-11 23:56:27 +0300

    Member area için DashboardController oluşturuldu.

---
+++ TraversalCoreProje/Areas/Member/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public DashboardController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.username = user.Name + " " + user.Surname;
+            ViewBag.userimage = user.ImageUrl;
+            ViewBag.memberPhone = user.PhoneNumber;
+            ViewBag.memberMail = user.Email;
+            return View();
+        }
+    }
+}
