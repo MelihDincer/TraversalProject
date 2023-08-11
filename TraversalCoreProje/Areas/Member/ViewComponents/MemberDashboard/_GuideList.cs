9b03ca8a5533024f27fda7e79358bee49e3734c6
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-11 23:56:46 +0300

    _GuideList  viewcomponent class oluşturuldu.

---
+++ TraversalCoreProje/Areas/Member/ViewComponents/MemberDashboard/_GuideList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace TraversalCoreProje.Areas.Member.ViewComponents.MemberDashboard
+{
+    public class _GuideList : ViewComponent
+    {
+        GuideManager guideManager = new GuideManager(new EfGuideDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = guideManager.TGetList().Take(5).ToList();
+            return View(values);
+        }
+    }
+}
