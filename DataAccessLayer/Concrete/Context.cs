a9b08ffa0b85b61a3f045bddbf5540cbe60bd252
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-11 23:50:32 +0300

    Reservations tablosu context sınıfına eklendi.

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -27,5 +27,6 @@ namespace DataAccessLayer.Concrete
         public DbSet<SubAbout> SubAbouts { get; set; }
         public DbSet<Testimonial> Testimonials { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
     }
 }
