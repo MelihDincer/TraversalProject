7e6123890225a87245d521e1cfe8abc23735b12a
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-12 22:49:49 +0300

    Dashboard sayfasında; istatistiklerin yer aldığı bölümler iki ayrı partialview olarak eklendi.

---
+++ TraversalCoreProje/Areas/Admin/ViewComponents/AdminDashboard/_Cards1Statistic.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace TraversalCoreProje.Areas.Admin.ViewComponents.AdminDashboard
+{
+    public class _Cards1Statistic : ViewComponent
+    {
+        Context c = new();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = c.Destinations.Count();
+            ViewBag.v2 = c.Users.Count();
+            return View();
+        }
+    }
+}
