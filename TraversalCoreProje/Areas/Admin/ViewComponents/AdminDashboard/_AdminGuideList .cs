63d83372c961d27b76caf3014efa27b15b462b8c
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-12 22:49:21 +0300

    Dashboard sayfasında; rehberlerin yer aldığı kısım partialview olarak eklendi.

---
+++ TraversalCoreProje/Areas/Admin/ViewComponents/AdminDashboard/_AdminGuideList .cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Admin.ViewComponents.AdminDashboard
+{
+    public class _AdminGuideList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
