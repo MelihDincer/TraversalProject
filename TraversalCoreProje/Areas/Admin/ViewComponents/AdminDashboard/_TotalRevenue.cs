d0936c3926ceb623c2dcc039e9ce5a74ae1c3993
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-12 23:04:04 +0300

    _TotalRevenue viewcomponenti eklendi.

---
+++ TraversalCoreProje/Areas/Admin/ViewComponents/AdminDashboard/_TotalRevenue.cs
@@ -0,0 +1,13 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Admin.ViewComponents.AdminDashboard
+{
+    public class _TotalRevenue : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
