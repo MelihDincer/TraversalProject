195693d09a6b3ae8f4f346376a50977c6cadbc90
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-15 17:30:02 +0300

    Projeye UserController eklendi.

---
+++ TraversalCoreProje/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly IAppUserService _appUserService;
+        private readonly IReservationService _reservationService;
+
+        public UserController(IAppUserService appUserService, IReservationService reservationService)
+        {
+            _appUserService = appUserService;
+            _reservationService = reservationService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _appUserService.TGetList();
+            return View(values);
+        }
+        public IActionResult DeleteUser(int id)
+        {
+            var values = _appUserService.TGetByID(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditUser(int id)
+        {
+            var values = _appUserService.TGetByID(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult EditUser(AppUser appUser)
+        {
+            _appUserService.TUpdate(appUser);
+            return RedirectToAction("Index");
+        }
+
+        //Kullanıcının yapmış olduğu yorumların listesi
+        public IActionResult CommentUser(int id)
+        {
+            _appUserService.TGetList();
+            return View();
+        }
+
+        //Kullanıcının geçmişte kayıt olduğu turların listesi
+        public IActionResult ReservationUser(int id)
+        {
+            var values = _reservationService.GetListWithReservationByAccepted(id);
+            var user = _appUserService.TGetByID(id);
+            ViewBag.username = user.Name + " " + user.Surname;
+            return View(values);
+        }
+    }
+}
