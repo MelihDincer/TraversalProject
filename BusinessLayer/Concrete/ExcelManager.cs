4947514459359ce57959451bd81cca5db01ca029
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-16 23:03:07 +0300

    ExcelManager oluşturuldu.

---
+++ BusinessLayer/Concrete/ExcelManager.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ExcelManager : IExcelService
+    {
+        public byte[] ExcelList<T>(List<T> t) where T : class
+        {
+            ExcelPackage excel = new();
+            var workSheet = excel.Workbook.Worksheets.Add("Sayfa1");
+            workSheet.Cells["A1"].LoadFromCollection(t, true, OfficeOpenXml.Table.TableStyles.Dark8);
+            return excel.GetAsByteArray();
+        }
+    }
+}
