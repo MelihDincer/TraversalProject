85cb8992a91b5c84e6b54df896b059c87ee57e83
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-16 23:08:56 +0300

    IGuideService'da rehberlerin pasif ve aktif yapılması için tanımlanan metot imzaları implemente edildi. TAdd metodunun işlevi tamamlandı.

--- BusinessLayer/Concrete/GuideManager.cs
+++ BusinessLayer/Concrete/GuideManager.cs
@@ -20,17 +20,27 @@ namespace BusinessLayer.Concrete
 
         public void TAdd(Guide t)
         {
-            throw new NotImplementedException();
+            _guideDal.Insert(t);
+        }
+
+        public void TChangeToFalseByGuide(int id)
+        {
+            _guideDal.ChangeToFalseByGuide(id);
+        }
+
+        public void TChangeToTrueByGuide(int id)
+        {
+            _guideDal.ChangeToTrueByGuide(id);
         }
 
         public void TDelete(Guide t)
         {
-            throw new NotImplementedException();
+            _guideDal.Delete(t);
         }
 
         public Guide TGetByID(int id)
         {
-            throw new NotImplementedException();
+            return _guideDal.GetByID(id);
         }
 
         public List<Guide> TGetList()
@@ -40,7 +50,7 @@ namespace BusinessLayer.Concrete
 
         public void TUpdate(Guide t)
         {
-            throw new NotImplementedException();
+            _guideDal.Update(t);
         }
     }
 }
