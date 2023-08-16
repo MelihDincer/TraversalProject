8e4246635b7784584751b6c14e29b5e869855c2e
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-16 23:19:27 +0300

    Mail gönderme işlemi için model oluşturuldu.

---
+++ TraversalCoreProje/Models/MailRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace TraversalCoreProje.Models
+{
+    public class MailRequest
+    {
+        public string Name { get; set; }
+        public string SenderMail { get; set; } //Gönderenin mail adresi
+        public string RecieverMail { get; set; } //Alıcının mail adresi
+        public string Subject { get; set; } //Konu
+        public string Body { get; set; } //Mesaj içeriği (paragraf)
+    }
+}
