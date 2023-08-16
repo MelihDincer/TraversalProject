027166c71a0ea8c6e522c6540c28108ebe1d315d
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-16 23:03:52 +0300

    PdfReportController oluşturuldu.

---
+++ TraversalCoreProje/Areas/Admin/Controllers/PdfReportController.cs
@@ -0,0 +1,64 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PdfReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult StaticPdfReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pdfreports/"+"dosya1.pdf");
+            var stream = new FileStream(path, FileMode.Create);
+
+            Document document = new Document(PageSize.A4);
+            PdfWriter.GetInstance(document, stream);
+
+            document.Open();
+
+            Paragraph paragraph = new Paragraph("Traversal Rezervasyon Pdf Raporu");
+
+            document.Add(paragraph);
+            document.Close();
+            return File("/pdfreports/dosya1.pdf", "application/pdf", "dosya1.pdf");
+        }
+
+        public IActionResult StaticCustomerReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pdfreports/" + "dosya2.pdf");
+            var stream = new FileStream(path, FileMode.Create);
+
+            Document document = new Document(PageSize.A4);
+            PdfWriter.GetInstance(document, stream);
+
+            document.Open();
+
+            PdfPTable pdfPTable = new PdfPTable(3);
+            pdfPTable.AddCell("Misafir Adı");
+            pdfPTable.AddCell("Misafir Soyadı");
+            pdfPTable.AddCell("Misafir TC");
+
+            pdfPTable.AddCell("Eylül");
+            pdfPTable.AddCell("Yücedağ");
+            pdfPTable.AddCell("11111111111");
+
+            pdfPTable.AddCell("Kemal");
+            pdfPTable.AddCell("Yıldırım");
+            pdfPTable.AddCell("22222222222");
+
+            pdfPTable.AddCell("Mehmet");
+            pdfPTable.AddCell("Yücedağ");
+            pdfPTable.AddCell("33333333333");
+
+            document.Add(pdfPTable);
+            document.Close();
+            return File("/pdfreports/dosya2.pdf", "application/pdf", "dosya2.pdf");
+        }
+    }
+}
