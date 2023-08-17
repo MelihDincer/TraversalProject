4b4006b8aaf608ec27bf368b0e3ee771709b6be3
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-17 16:39:38 +0300

    ContactUsManager oluşturuldu. Durum bilgisiyle alakalı tanımlanan metot imzaları implemente edildi.

---
+++ BusinessLayer/Concrete/ContactUsManager.cs
@@ -0,0 +1,61 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ContactUsManager : IContactUsService
+    {
+        IContactUsDal _contactUsDal;
+
+        public ContactUsManager(IContactUsDal contactUsDal)
+        {
+            _contactUsDal = contactUsDal;
+        }
+
+        public void ContactUsStatusChangeToFalse(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ContactUs> GetListContactUsByFalse()
+        {
+            return _contactUsDal.GetListContactUsByFalse();
+        }
+
+        public List<ContactUs> GetListContactUsByTrue()
+        {
+            return _contactUsDal.GetListContactUsByTrue();
+        }
+
+        public void TAdd(ContactUs t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(ContactUs t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ContactUs TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ContactUs> TGetList()
+        {
+            return _contactUsDal.GetList();
+        }
+
+        public void TUpdate(ContactUs t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
