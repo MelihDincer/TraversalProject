5e980d4524e4a5fe015dc68fa35802012fd90b30
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-17 17:25:11 +0300

    Farklı sınıflardaki nesneleri eşlemek için AutoMapper uygulaması yapıldı. MapProfile sınıfı eklenerek Profile sınıfından miras alındı. Constructor içerisinde eşleme işlemleri yapıldı.

---
+++ TraversalCoreProje/Mapping/AutoMapperProfile/MapProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using DTOLayer.DTOs.AnnouncementDTOs;
+using DTOLayer.DTOs.AppUserDTOs;
+using EntityLayer.Concrete;
+
+namespace TraversalCoreProje.Mapping.AutoMapperProfile
+{
+    public class MapProfile : Profile
+    {
+        //AutoMapper; farklı sınıflardaki nesneleri eşlemek için kullanılan ve nesneden nesneye eşlemeyi sağlayan popüler bir kütüphanedir.
+        public MapProfile()
+        {
+            //CreateMap<AnnouncementAddDTOs, Announcement>();
+            //CreateMap<Announcement, AnnouncementAddDTOs>();
+            //Yukarıdaki iki satır kod yerine tek satırda aşağıdaki gibi çözüm sağlayabiliriz. İkisi de aynı amaca hizmet etmektedir.
+            CreateMap<AnnouncementAddDto, Announcement>().ReverseMap();
+            CreateMap<AppUserRegisterDto, AppUser>().ReverseMap(); // DTO ismi , entity ismi
+            CreateMap<AppUserLoginDto, AppUser>().ReverseMap();
+            CreateMap<AnnouncementListDto, Announcement>().ReverseMap();  
+            CreateMap<AnnouncementUpdateDto, Announcement>().ReverseMap();  
+        }
+    }
+}
