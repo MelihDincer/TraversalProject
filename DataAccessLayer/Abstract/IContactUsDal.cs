1e302d15936e62f1b62ae40143de52c83c66eaaa
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-17 17:25:36 +0300

    IContactUsDal oluşturuldu.

---
+++ DataAccessLayer/Abstract/IContactUsDal.cs
@@ -0,0 +1,16 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IContactUsDal : IGenericDal<ContactUs>
+    {
+        List<ContactUs> GetListContactUsByTrue(); //Statusu true olanları getir.
+        List<ContactUs> GetListContactUsByFalse(); //Statusu false olanları getir.
+        void ContactUsStatusChangeToFalse(int id); //Statusu false yap.
+    }
+}
