4296dbd5af1872e1d429930f55132a9e424a6e24
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-17 16:29:07 +0300

    MailRequestDto eklendi.

---
+++ DTOLayer/DTOs/MailDTOs/MailRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOLayer.DTOs.MailDTOs
+{
+    public class MailRequestDto
+    {
+        public string Name { get; set; }
+        public string SenderMail { get; set; } //Gönderenin mail adresi
+        public string RecieverMail { get; set; } //Alıcının mail adresi
+        public string Subject { get; set; } //Konu
+        public string Body { get; set; } //Mesaj içeriği (paragraf)
+    }
+}
