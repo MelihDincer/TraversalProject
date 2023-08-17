62632e3509c73b68c7ac0286e4c263f9950890e9
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-17 16:38:06 +0300

    IContactUsService oluşturuldu. Durum bilgisiyle alakalı 3 adet metot imzası tanımlandı.

---
+++ BusinessLayer/Abstract/IContactUsService.cs
@@ -0,0 +1,16 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IContactUsService : IGenericService<ContactUs>
+    {
+        List<ContactUs> GetListContactUsByTrue(); //Statusu true olanları getir.
+        List<ContactUs> GetListContactUsByFalse(); //Statusu false olanları getir.
+        void ContactUsStatusChangeToFalse(int id); //Statusu false yap.
+    }
+}
