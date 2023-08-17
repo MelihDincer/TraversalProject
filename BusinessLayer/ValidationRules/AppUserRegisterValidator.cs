0f9565767e4f4cbbebee4d166bac58155f976618
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-17 16:45:20 +0300

    AppUserRegisterValidator eklendi.

---
+++ BusinessLayer/ValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,26 @@
+﻿using DTOLayer.DTOs.AppUserDTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı boş geçilemez");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad alanı boş geçilemez");
+            RuleFor(x => x.Mail).NotEmpty().WithMessage("Mail alanı boş geçilemez");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanıcı adı alanı boş geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre alanı boş geçilemez");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şifre tekrar alanı boş geçilemez");
+            RuleFor(x => x.Username).MinimumLength(5).WithMessage("Lütfen en az 5 karakter veri girişi yapınız");
+            RuleFor(x => x.Username).MaximumLength(20).WithMessage("Lütfen en fazla 20 karakter veri girişi yapınız");
+            RuleFor(x => x.Password).Equal(x => x.ConfirmPassword).WithMessage("Şifreler eşleşmemektedir");
+        }
+    }
+}
