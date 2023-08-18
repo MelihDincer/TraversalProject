6dda98206519dfd53e9cd8a633a277ce4e2543ca
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-18 23:59:50 +0300

    Kur bilgisi için api controller oluşturuldu

---
+++ TraversalCoreProje/Areas/Admin/Controllers/ApiExchangeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using TraversalCoreProje.Areas.Admin.Models;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ApiExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<BookingExchangeViewModel2> bookingExchangeViewModels = new List<BookingExchangeViewModel2>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://apidojo-booking-v1.p.rapidapi.com/currency/get-exchange-rates?base_currency=TRY&languagecode=en-us"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "4989b69e0bmsh169542d11e74854p1241b2jsnf9e27d1a6ff4" },
+        { "X-RapidAPI-Host", "apidojo-booking-v1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<BookingExchangeViewModel2>(body);
+                return View(values.exchange_rates);
+
+            }
+            
+        }
+    }
+}
