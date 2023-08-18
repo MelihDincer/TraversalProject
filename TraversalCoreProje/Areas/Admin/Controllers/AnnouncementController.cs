6a171d28a0c2ff747e5fd9f2b48319505cb7faa9
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-18 23:58:48 +0300

    AnnouncementController oluşturuldu.

---
+++ TraversalCoreProje/Areas/Admin/Controllers/AnnouncementController.cs
@@ -0,0 +1,85 @@
+﻿using AutoMapper;
+using BusinessLayer.Abstract;
+using DTOLayer.DTOs.AnnouncementDTOs;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("/Admin/[controller]/[action]")]
+    public class AnnouncementController : Controller
+    {
+        private readonly IAnnouncementService _announcementService;
+        private readonly IMapper _mapper;
+
+        public AnnouncementController(IAnnouncementService announcementService, IMapper mapper)
+        {
+            _announcementService = announcementService;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _mapper.Map<List<AnnouncementListDto>>(_announcementService.TGetList());
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddAnnouncement()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddAnnouncement(AnnouncementAddDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                _announcementService.TAdd(new Announcement()
+                {
+                    Content = model.Content,
+                    Title = model.Title,
+                    Date = Convert.ToDateTime(DateTime.Now.ToShortDateString())
+                });
+                return RedirectToAction("Index", "Announcement", new { area = "Admin" });
+            }
+            return View(model);
+        }
+
+        [Route("{id}")]
+        public IActionResult DeleteAnnouncement(int id)
+        {
+            var values = _announcementService.TGetByID(id);
+            _announcementService.TDelete(values);
+            return RedirectToAction("Index", "Announcement", new { area = "Admin" });
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public IActionResult UpdateAnnouncement(int id)
+        {
+            var values = _mapper.Map<AnnouncementUpdateDto>(_announcementService.TGetByID(id));
+            return View(values);
+        }
+        [Route("{id}")]
+        [HttpPost]
+        public IActionResult UpdateAnnouncement(AnnouncementUpdateDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                _announcementService.TUpdate(new Announcement
+                {
+                    AnnouncementID = model.AnnouncementID,
+                    Title = model.Title,
+                    Content = model.Content,
+                    Date = Convert.ToDateTime(DateTime.Now.ToShortDateString())
+                });
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
