bb8352ba4f9b6fa8b0cd03c716ea6eb829d855a2
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-19 00:00:06 +0300

    Ziyaretçi bilgisi için api controller oluşturuldu

---
+++ TraversalCoreProje/Areas/Admin/Controllers/VisitorApiController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TraversalCoreProje.Areas.Admin.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    //Burası Api'yi consume edecek
+    [Area("Admin")]
+    [Route("/Admin/[controller]/[action]")]
+    public class VisitorApiController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public VisitorApiController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:62968/api/Visitor");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<VisitorViewModel>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddVisitor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddVisitor(VisitorViewModel p)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(p);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:62968/api/Visitor", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Visitor", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteVisitor(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:62968/api/Visitor/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Visitor", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateVisitor(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:62968/api/Visitor/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<VisitorViewModel>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [Route("{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateVisitor(VisitorViewModel p)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(p);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:62968/api/Visitor", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Visitor", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
