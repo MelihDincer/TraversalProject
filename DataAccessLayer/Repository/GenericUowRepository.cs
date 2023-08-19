c6f2cf08b19b844887de06d23d1dc72ebb163645
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-19 19:43:47 +0300

    GenericUowRepository oluşturuldu.

---
+++ DataAccessLayer/Repository/GenericUowRepository.cs
@@ -0,0 +1,40 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class GenericUowRepository<T> : IGenericUowDal<T> where T : class
+    {
+        private readonly Context _context;
+
+        public GenericUowRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public T GetByID(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Insert(T t)
+        {
+            _context.Add(t);
+        }
+
+        public void MultiUpdate(List<T> t)
+        {
+            _context.UpdateRange(t); //UpdateRange metodu ile aynı anda birden fazla değere göre güncelleştirme şansı yakaladık.
+        }
+
+        public void Update(T t)
+        {
+            _context.Update(t);
+        }
+    }
+}
