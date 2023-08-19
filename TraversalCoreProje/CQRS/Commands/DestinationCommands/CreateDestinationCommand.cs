b802a99f2f94c3bf409f56c8d307d056dcc6fddf
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-19 10:28:46 +0300

    CQRS ile destinasyon ekleme işlemi için command ve handler oluşturuldu.

---
+++ TraversalCoreProje/CQRS/Commands/DestinationCommands/CreateDestinationCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace TraversalCoreProje.CQRS.Commands.DestinationCommands
+{
+    public class CreateDestinationCommand
+    {
+        public string City { get; set; }
+        public string DayNight { get; set; }
+        public double Price { get; set; }
+        public int Capacity { get; set; }
+    }
+}
---
+++ TraversalCoreProje/CQRS/Handlers/DestinationHandlers/CreateDestinationCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using TraversalCoreProje.CQRS.Commands.DestinationCommands;
+
+namespace TraversalCoreProje.CQRS.Handlers.DestinationHandlers
+{
+    public class CreateDestinationCommandHandler
+    {
+        private readonly Context _context;
+
+        public CreateDestinationCommandHandler(Context context)
+        {
+            _context = context;
+        }
+        
+        //Mediator kullandığımızda da default olarak "Handle" ismini alacaktır.
+        public void Handle(CreateDestinationCommand command)
+        {
+            _context.Destinations.Add(new Destination
+            {
+                City = command.City,
+                Capacity = command.Capacity,
+                Price = command.Price,
+                DayNight = command.DayNight,
+                Status = true
+            });
+            _context.SaveChanges();
+        }
+    }
+}
