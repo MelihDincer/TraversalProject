adb1ac5ec253e66766f6139e6716a44e24fe68b3
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-19 10:22:35 +0300

    Projede CQRS Design Pattern uygulaması için CQRS klasörü oluşturuldu. Result klasörü içerisinde destinasyonları listeleme ve güncelleme sayfasında getirilecek propertyler ilgili sınıfların içerisinde tanımlandı.

---
+++ TraversalCoreProje/CQRS/Results/DestinationResults/GetAllDestinationQueryResult.cs
@@ -0,0 +1,12 @@
+﻿namespace TraversalCoreProje.CQRS.Results.DestinationResults
+{
+    //Entitymize karşılık gelen property leri tutacak kısım.
+    public class GetAllDestinationQueryResult
+    {
+        public int id { get; set; }
+        public string city { get; set; }
+        public string daynight { get; set; }
+        public double price { get; set; }
+        public int capacity { get; set; }
+    }
+}
