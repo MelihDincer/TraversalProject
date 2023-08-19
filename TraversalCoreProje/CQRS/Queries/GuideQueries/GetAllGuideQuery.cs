07def561510330ba562675e9d2ab062129841c13
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-19 20:11:08 +0300

    GetAllGuideQueryResult oluşturuldu.

---
+++ TraversalCoreProje/CQRS/Queries/GuideQueries/GetAllGuideQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using TraversalCoreProje.CQRS.Results.GuideResults;
+
+namespace TraversalCoreProje.CQRS.Queries.GuideQueries
+{
+    public class GetAllGuideQuery : IRequest<List<GetAllGuideQueryResult>>
+    {
+    }
+}
---
+++ TraversalCoreProje/CQRS/Queries/GuideQueries/GetGuideByIDQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using TraversalCoreProje.CQRS.Results.GuideResults;
+
+namespace TraversalCoreProje.CQRS.Queries.GuideQueries
+{
+    public class GetGuideByIDQuery : IRequest<GetGuideByIDQueryResult>
+    {
+        public GetGuideByIDQuery(int ıd)
+        {
+            Id = ıd;
+        }
+
+        public int Id { get; set; }
+    }
+}
