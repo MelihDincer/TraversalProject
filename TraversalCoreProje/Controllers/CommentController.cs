9fbc91165c0780e4f4063f7aa8fd5edee58a7c7c
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-20 23:49:16 +0300

    Yorum ekleme kısmı kayıtlı kullanıcılara özel yapılacağı için ve destinasyon tablosuyla ilişkili olduğundan yorum ekleme işlemi Destination Controllerda yapılacağından buradaki id parametresine gerek kalmadı ve silindi.

--- TraversalCoreProje/Controllers/CommentController.cs
+++ TraversalCoreProje/Controllers/CommentController.cs
@@ -1,19 +1,23 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Threading.Tasks;
 
 namespace TraversalCoreProje.Controllers
 {
+    [AllowAnonymous]
     public class CommentController : Controller
     {
         CommentManager commentManager = new CommentManager(new EfCommentDal());
 
+
         [HttpGet]
-        public PartialViewResult AddComment(int id)
+        public  PartialViewResult AddComment()
         {
-            ViewBag.i = id;
             return PartialView();
         }
         [HttpPost]
