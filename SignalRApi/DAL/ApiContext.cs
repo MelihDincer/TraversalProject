ef4b864304f2f7caa9f7366a9e083b450c235f20
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-22 22:29:11 +0300

    Context sınıfı oluşturuldu.

---
+++ SignalRApi/DAL/ApiContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace SignalRApi.DAL
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options): base(options) { }
+
+        //protected readonly IConfiguration Configuration;
+        //public ApiContext(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+        //}
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
