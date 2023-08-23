0877efbc7b9fd61142732b9cb0ffbd0df42e945c
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-23 22:32:31 +0300

    Küçük bir hata kaynaklı Apicontext silindi. Yerine ApiAppContext eklendi.

---
+++ SignalRApi/DAL/ApiAppContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace SignalRApi.DAL
+{
+    public class ApiAppContext : DbContext
+    {
+        public ApiAppContext(DbContextOptions<ApiAppContext> options): base(options) { }
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
