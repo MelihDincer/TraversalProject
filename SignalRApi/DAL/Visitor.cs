bda4e1be8c0b1ca1195909d81a2b78263729dcfd
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-23 22:42:52 +0300

    Visitor sınıfı revize edildi.

--- SignalRApi/DAL/Visitor.cs
+++ SignalRApi/DAL/Visitor.cs
@@ -6,15 +6,15 @@ namespace SignalRApi.DAL
     {
         Edirne = 1,
         İstanbul = 2,
-        Ankara = 3, 
+        Ankara = 3,
         Antalya = 4,
         Bursa = 5
     }
     public class Visitor
     {
         public int VisitorID { get; set; }
-        public ECity City{ get; set; }
-        public int CityVisitCount{ get; set; }
-        public DateTime VisitDate{ get; set; }
+        public ECity City { get; set; }
+        public int CityVisitCount { get; set; }
+        public DateTime VisitDate { get; set; }
     }
 }
