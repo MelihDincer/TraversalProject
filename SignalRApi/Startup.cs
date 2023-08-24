50e7fc74d95bd78fec9047d38178f59bf310b4ca
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-24 13:42:33 +0300

    Consume işlemi için startup içerisinde servis ve configure düzenlemeleri yapıldı.

--- SignalRApi/Startup.cs
+++ SignalRApi/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using SignalRApi.DAL;
+using SignalRApi.DAL.Hubs;
 using SignalRApi.Model;
 using System;
 using System.Collections.Generic;
@@ -30,9 +31,20 @@ namespace SignalRApi
         {
             services.AddScoped<VisitorService>();
             services.AddSignalR();
+
+            //AddPolicy metodu ile cors policy oluþturuldu. Bu policy aþaðýda Configure içerisinde çaðrýlmasý gerekmektedir.
+            services.AddCors(options => options.AddPolicy("CorsPolicy", //Consume üzerinden server ý tüketmemize olanak saðlayan metot.
+                builder =>
+                {
+                    builder.AllowAnyHeader()     //Dýþarýdan herhangi bir baþlýðýn gelmesine izin ver.
+                    .AllowAnyMethod()      //Dýþarýdan herhangi bir metodun gelmesine izin ver.
+                    .SetIsOriginAllowed((host) => true)
+                    .AllowCredentials();
+                }));
+
             services.AddEntityFrameworkNpgsql().AddDbContext<ApiAppContext>(opt =>
             opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
-            
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -51,12 +63,13 @@ namespace SignalRApi
             }
 
             app.UseRouting();
-
+            app.UseCors("CorsPolicy"); // Yukarýda oluþturduðumuz corspolicy burada çaðrýldý.
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<VisitorHub>("/VisitorHub");   //Nereyi tüketeceðimiz burada belirtilir.Syntax tam olarak bu þekilde.
             });
         }
     }
