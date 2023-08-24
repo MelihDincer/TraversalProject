9453086304293537d36fd4bea6dd5a426d4953eb
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-24 21:56:12 +0300

    Consume işlemi için iki ayrı sayfa eklendi.

--- SignalRConsume/Controllers/HomeController.cs
+++ SignalRConsume/Controllers/HomeController.cs
@@ -22,6 +22,14 @@ namespace SignalRConsume.Controllers
         {
             return View();
         }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        public IActionResult Index3()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
