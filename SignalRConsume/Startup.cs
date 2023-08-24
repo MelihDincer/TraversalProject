76e2a3a0402ccacbeac07e1ce96f292a0ee1c751
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-24 12:44:06 +0300

    Bir önceki eklenen SignalRConsume projesi mvc olmadığından, silinip mvc projesi eklendi.

--- SignalRConsume/Startup.cs
+++ SignalRConsume/Startup.cs
@@ -22,7 +22,7 @@ namespace SignalRConsume
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,9 +34,8 @@ namespace SignalRConsume
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
             }
-
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -45,7 +44,9 @@ namespace SignalRConsume
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
