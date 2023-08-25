5ea7c39bfcd553de9db1ec8fb6136230ecf4c2a0
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-25 16:10:19 +0300

    RoleController oluşturdu. Rol işlemleri için rol oluşturma, rolleri listeleme ve silme işlemleri yapıldı.

---
+++ TraversalCoreProje/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,113 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TraversalCoreProje.Areas.Admin.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("/Admin/[controller]/[action]")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public RoleController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _roleManager.Roles.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel createRoleViewModel)
+        {
+            AppRole role = new AppRole()
+            {
+                Name = createRoleViewModel.RoleName
+            };
+            var result = await _roleManager.CreateAsync(role);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Role", new { area = "Admin" });
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            var rolevalues = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
+            await _roleManager.DeleteAsync(rolevalues);
+            return RedirectToAction("Index", "Role", new { area = "Admin" });
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult UpdateRole(int id)
+        {
+            var value = _roleManager.Roles.FirstOrDefault(x=>x.Id == id);
+            UpdateRoleViewModel updateRoleViewModel = new UpdateRoleViewModel
+            {
+                RoleID = value.Id,
+                RoleName = value.Name,
+            };
+            return View(updateRoleViewModel);
+        }
+
+        [HttpPost]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateRole(UpdateRoleViewModel updateRoleViewModel)
+        {
+            var value = _roleManager.Roles.FirstOrDefault(x => x.Id == updateRoleViewModel.RoleID);
+            value.Name = updateRoleViewModel.RoleName;
+            await _roleManager.UpdateAsync(value);
+            return RedirectToAction("Index", "Role", new { area = "Admin" });
+        }
+
+        public IActionResult UserList()
+        {
+            var values = _userManager.Users.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> AssignRole(List<RoleAssignViewModel> model)
+        {
+            var userid = (int)TempData["userid"];
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == userid);
+            foreach (var item in model)
+            {
+                if (item.RoleExist)
+                {
+                    await _userManager.AddToRoleAsync(user, item.RoleName);
+                }
+                else
+                {
+                    await _userManager.RemoveFromRoleAsync(user, item.RoleName);
+                }
+            }
+            return RedirectToAction("UserList");
+        }
+    }
+}
+
