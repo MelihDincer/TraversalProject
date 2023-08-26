89b9a0edbfda18582295745c00444e37e23f6d3f
Melih Dinçer <m_dincer41@hotmail.com>
2023-08-26 14:03:44 +0300

    Son 4 destinasyon verileri için business da metot imzası tanımlandı.

--- BusinessLayer/Abstract/IDestinationService.cs
+++ BusinessLayer/Abstract/IDestinationService.cs
@@ -10,5 +10,7 @@ namespace BusinessLayer.Abstract
     public interface IDestinationService : IGenericService<Destination>
     {
         public Destination TGetDestinationWithGuide(int id);
+        public List<Destination> TGetLast4Destinations();
+
     }
 }
